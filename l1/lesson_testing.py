39f3d5a0f5384b46ec841ad7866982cadf8ff111
Cherkasov Kirill <cherkasov.kirill@gmail.com>
2018-05-29 09:52:07 +0500

    Download homework.

--- l1/lesson_testing.py
+++ l1/lesson_testing.py
@@ -2,6 +2,7 @@ from collections import namedtuple
 import doctest
 import datetime
 import unittest
+import pytest
 
 DATA = '''
 Фамилия     Имя     Часов       Ставка
@@ -20,7 +21,7 @@ def salary_calc(line):
     >>> salary_calc('Ромашкин    Филимон 20          1000')
     ('Ромашкин Филимон', 20000)
     >>> salary_calc('Иванов      Иван    45          5000')
-    ('Иванов Иван', 224000)
+    ('Иванов Иван', 225000)
     """
     if line:
         data = Salary(*line.split())
@@ -39,8 +40,10 @@ def test_empty_str():
     assert salary_calc('') == (), 'Непустые данные'
 
 
-def test_fio():
-    assert salary_calc('Ромашкин    Филимон 20          1000')[0] == 'Ромашкин Филимон', 'Неверный ФИО'
+@pytest.mark.parametrize('data, expected', [('Ромашкин    Филимон 20          1000', ('Ромашкин Филимон', 20000)),
+                                            ('Иванов      Иван    45          5000', ('Иванов Иван', 225000))])
+def test_fio(data, expected):
+    assert salary_calc(data) == expected, 'Неверный ФИО'
 
 
 def test_salary():
@@ -54,10 +57,9 @@ class Employee:
     """class Employee
     >>> e = Employee('Ромашкин Филимон', 1990)
     >>> e.age
-    27
+    28
     >>> print(e)    # doctest: +NORMALIZE_WHITESPACE
-    'Ромашкин ИванРомашкин ИванРомашкин ИванРомашкин ИванРомашкин ИванРомашкин ИванРомашкин ИванРомашкин ИванРомашкин
-    ИванРомашкин ИванРомашкин ИванРомашкин ИванРомашкин ИванРомашкин ИванРомашкин Иван'
+    Ромашкин ФилимонРомашкин ФилимонРомашкин ФилимонРомашкин ФилимонРомашкин ФилимонРомашкин ФилимонРомашкин ФилимонРомашкин ФилимонРомашкин ФилимонРомашкин ФилимонРомашкин ФилимонРомашкин ФилимонРомашкин ФилимонРомашкин ФилимонРомашкин Филимон
     """
     def __init__(self, name, birth_year):
         self.name = name
@@ -74,7 +76,14 @@ class Employee:
 # unittest
 class TestSalary(unittest.TestCase):
     def test_salary_summ(self):
-        pass
+        self.assertEqual(salary_calc('Ромашкин    Филимон 20          1000')[1], 20000, "Неверная сумма")
+
+    def test_salary_fio(self):
+        self.assertIn('Ромашкин Филимон',salary_calc('Ромашкин    Филимон 20          1000'), "Неверное ФИО")
+
+    def test_salary_exception(self):
+        with self.assertRaises(ValueError):
+            salary_calc('x x x x')
 
 # print(salary_calc('Ромашкин    Филимон 20          1000'))
 # print(salary_calc('Иванов      Иван    45          5000'))
@@ -85,7 +94,7 @@ class TestSalary(unittest.TestCase):
 
 if __name__ == '__main__':
     test_empty_str()
-    test_fio()
     test_salary()
     # tests all >>> expressions in function documentation
     doctest.testmod()
+    unittest.main()
