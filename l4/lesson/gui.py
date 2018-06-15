d194bf12ea782fa29a1f602e2efa11d4f04aa58e
Kirill C <cherkasov.kirill@gmail.com>
2018-06-15 16:02:53 +0500

    Stop watching at 1:58:59.

--- l4/lesson/gui.py
+++ l4/lesson/gui.py
@@ -2,18 +2,34 @@ from tkinter import *
 from tkinter.messagebox import *
 import os
 
+
+def sql_get_terminals():
+    ls = []
+    for i in range(3):
+        ls.append([i, 'Terminal_{}'.format(i), 'description'])
+    return ls
+
+
 class TableGrid(Frame):
     def __init__(self, parent=None, titles=None, rows=0, *args):
         super().__init__(parent)
-
+        self.vars = []
         for index, title in enumerate(titles):
             Label(self, text=title).grid(row=0, column=index)
 
         for i in range(1, rows+1):
+            self.vars.append([])
             for j in range(index+1):
-                Entry(self).grid(row=i, column=j)
+                var = StringVar()
+                var.set('hello')
+                Entry(self, textvariable=var).grid(row=i, column=j)
+                self.vars[i-1].append(var)
         self.pack()
 
+    def get_terminals(self):
+        for index, data in enumerate(sql_get_terminals()):
+            for i, d in enumerate(data):
+                self.vars[index][i].set(d)
 
 def not_implemented(x=13):
     print('Not implemented yet!', x)
@@ -25,6 +41,7 @@ def not_implemented_2(event):
 
 main_window = Tk()
 main_window.minsize(400, 400)
+grid = TableGrid(main_window, ('Id', 'Name', 'Description'), 3)
 main_window.resizable(width=False, height=False)
 main_window.title('Star administrator')
 # fr1 = Frame(main_window)
@@ -38,6 +55,8 @@ for i in range(5):
     btn = Button(fr2, text='Create DB', command=lambda x=i: not_implemented(x))
     btn.grid(row=0, column=i)
 fr2.pack(side=BOTTOM)
+btn = Button(fr3, text='Get terminals', command=sql_get_terminals)
+btn.pack()
 
 # left, wheel, right  mouse buttons accordingly
 main_window.bind('<Button-1>', not_implemented_2)
@@ -45,8 +64,7 @@ main_window.bind('<Button-2>', not_implemented_2)
 main_window.bind('<Button-3>', not_implemented_2)
 main_menu = Menu(main_window)
 file_menu = Menu(main_menu )
-file_menu.add_command(label='Terminals', command=not_implemented)
+file_menu.add_command(label='Terminals', command=lambda g=grid: g.get_terminals())
 main_menu.add_cascade(label='Database', menu=file_menu)
 main_window.config(menu=main_menu)
-grid = TableGrid(main_window, ('Id', 'Name', 'Description'), 3)
 mainloop()
