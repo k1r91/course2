3df824e3c9d4a86e044b9144d7fcae4dc5212ca2
Cherkasov Kirill <cherkasov.kirill@gmail.com>
2018-06-26 11:53:52 +0500

    TODO: continue terminal gui.

--- payment_system/tk_gui/tk_term.py
+++ payment_system/tk_gui/tk_term.py
@@ -1,8 +1,11 @@
 import os
+import threading
+import time
 from tkinter import *
 from PIL import ImageTk, Image
 sys.path.append('..')
 from terminal import Terminal, TerminalException
+from transaction import PaymentTransaction
 import sql
 
 
@@ -28,23 +31,30 @@ class TkTerminal(Terminal):
         self.h = int(self.root.winfo_screenheight() * 2 / 3)
         self.w = int(self.root.winfo_screenwidth() * 2 / 3)
         self.root.geometry('{}x{}+{}+{}'.format(self.w, self.h, self.ws, self.hs))
-        self.display = Display(self.root, height=self.h-200, width=self.w)
+        self.display = Display(self, height=self.h-200, width=self.w)
         self.display.grid(row=0, columnspan=3)
         self.check_printer = CheckPrinter(self.root)
-        self.check_printer.grid(row=1, column=0, pady=self.h//30, padx=self.w//30, sticky='nw')
+        self.check_printer.grid(row=1, column=0, pady=self.h//40, padx=self.w//30, sticky='nw')
         self.strongbox = StrongBox(self.root)
-        self.strongbox.grid(row=1, column=1, sticky='nw', pady=self.h//30, padx=self.w//30)
+        self.strongbox.grid(row=1, column=1, sticky='nw', pady=self.h//40, padx=self.w//30)
         self.bill_acceptor = BillAcceptor(self.root)
-        self.bill_acceptor.grid(row=1, column=2, sticky='ws', pady=self.h // 30, padx=self.w // 30)
+        self.bill_acceptor.grid(row=1, column=2, sticky='ws', pady=self.h // 40, padx=self.w // 30)
         settings_img = get_resized_image(os.path.join('gif', 'settings.gif'), 20, 20)
         self.settings_btn = Button(self.root, image=settings_img, command=self.open_service)
-        self.settings_btn.grid(row=2, column=0, sticky='nw')
+        self.settings_btn.grid(row=2, column=0, sticky='nw', padx=10)
         self.settings_btn.image = settings_img
         self.items = []
-        self.display.load_organizations()
+        self.display.display_organizations()
 
     def open_service(self):
         print('Not implemented yet!')
+
+    def activate_bill_acceptor(self, data, amount, account):
+        self.bill_acceptor.activate()
+        print(data, amount, account)
+
+    def disable_bill_acceptor(self):
+        self.bill_acceptor.disable()
 
     def mainloop(self):
         self.root.mainloop()
@@ -54,18 +64,20 @@ class Display(Frame):
 
     def __init__(self, parent, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        parent.update()
-        self.width = parent.winfo_width()
-        self.height = parent.winfo_height()-200
+        self.parent = parent.root
+        self.terminal = parent
+        self.parent.update()
+        self.width = self.parent.winfo_width()
+        self.height = self.parent.winfo_height()-200
         background_image = get_resized_image(os.path.join('gif', 'display.gif'), self.width,
                                              self.height)
         self.frame = Label(self, image=background_image)
         self.frame.place(x=0, y=0, relwidth=1, relheight=1)
         self.frame.image = background_image
         self.types = []
-        self.orgs = []
+        self.elements = []
 
-    def load_organizations(self):
+    def display_organizations(self):
         self.display_types()
         self.change_page(1)
 
@@ -82,28 +94,25 @@ class Display(Frame):
             self.types.append(btn)
 
     def change_page(self, page):
-        for btn in self.types:
-            btn.config(fg='black')
+        self.flush()
+        self.display_types()
         self.types[page-1].config(fg='red')
-        for org in self.orgs:
-            org.destroy()
         data = sql.get_org_by_type(page)
-        print(data)
         row = 1
         col = 0
         width = self.width // 6
         height = self.height // 4
-        print(width, height)
         for i, item in enumerate(data):
             bfr = Frame(self.frame, width=width, height=height)
             filename = item[3].split('/')[-1].replace('.png', '.gif')
             img_path = os.path.join('gif', *item[3].split('/')[:-1], filename)
+            btn = Button(bfr, command=lambda x=item: self.display_pay_page(x))
             try:
                 image = get_resized_image(img_path, width, height)
-                btn = Button(bfr, image=image, command=lambda x=item: self.change_page_topay(x))
+                btn.config(image=image)
                 btn.image = image
             except FileNotFoundError:
-                btn = Button(bfr, text=item[1], command=lambda x=item: self.change_page_topay(x))
+                btn.config(text=item[1])
             if not i % 5:
                 col = 0
                 row += 1
@@ -114,24 +123,73 @@ class Display(Frame):
             bfr.grid(row=row, column=col, pady=10, padx=padx, sticky='nw')
             btn.place(x=0, y=0, relwidth=1, relheight=1)
             col += 1
+            self.elements.append(bfr)
 
-    def change_page_topay(self, org_data):
-        """
-        must be threaded
-        :param org_data:
-        :return:
-        """
-        print('Not implemented yet {}'.format(org_data))
+    def display_pay_page(self, data):
+        self.flush()
+        self.frame.columnconfigure(0, weight=1)
+        btn_main = Button(self.frame, text="To main page", width=15, command=lambda x=1: self.change_page(x))
+        btn_main.grid(row=0, column=0, padx=50, pady=10, sticky='nw')
+        phead = Label(self.frame, text='Pay for organization {}({})'.format(data[1], data[2]))
+        phead.config(font=('Courier', 22), fg='blue')
+        phead.grid(row=1, column=0, padx=50, pady=50, sticky='nwes')
+        self.accvar = StringVar()
+        self.amountvar = StringVar()
+        paccl = Label(self.frame, text='Input your personal account: ', font=('Courier', 12), fg='blue')
+        paccl.grid(row=2, column=0)
+        self.paccl_entry = Entry(self.frame, textvariable=self.accvar, width=11)
+        self.paccl_entry.grid(row=3, column=0)
+        pamountl = Label(self.frame, text='Input amount: ', font=('Courier', 12), fg='blue')
+        pamountl.grid(row=4, column=0)
+        self.pamountl_entry = Entry(self.frame, textvariable=self.amountvar, width=11)
+        self.pamountl_entry.grid(row=5, column=0)
+        bfr = Frame(self.frame, width=300)
+        self.error_label = Label(bfr, fg='red', font=('Courier', 12))
+        self.error_label.grid(row=0, columnspan=2)
+        submit = Button(bfr, text='OK', font=('Courier', 14), fg='blue', width=10,
+                        command=lambda x=data: self.threaded_pay(x))
+        submit.grid(row=1, column=0, padx=10)
+        cancel = Button(bfr, text='Cancel', font=('Courier', 14), fg='blue', width=10,
+                        command=lambda x=1: self.change_page(x))
+        cancel.grid(row=1, column=1)
+        bfr.grid(row=7, column=0, pady=20)
+        self.elements.append(btn_main)
+        self.elements.append(phead)
+        self.elements.append(paccl)
+        self.elements.append(self.paccl_entry)
+        self.elements.append(pamountl)
+        self.elements.append(self.pamountl_entry)
+        self.elements.append(bfr)
 
+    def threaded_pay(self, data):
+        self.error_label.config(text='')
+        try:
+            amount, account = int(self.amountvar.get()), int(self.accvar.get())
+        except ValueError:
+            self.error_label.config(text='Please, input numbers.')
+            return
+        if amount < PaymentTransaction.MIN_AMOUNT // 100:
+            self.error_label.config(text='Min pay {} rubles'.format(PaymentTransaction.MIN_AMOUNT // 100))
+            return
+        if amount > PaymentTransaction.MAX_AMOUNT // 100:
+            self.error_label.config(text='Max pay {} rubles.'.format(PaymentTransaction.MAX_AMOUNT // 100))
+        self.paccl_entry.config(state=DISABLED)
+        self.pamountl_entry.config(state=DISABLED)
+        task = threading.Thread(target=self.terminal.activate_bill_acceptor, args=(data, amount, account))
+        task.start()
     def flush(self):
         """
         eliminates types and organization buttons
         :return:
         """
-        for btype in self.types:
-            btype.destroy()
-        for org in self.orgs:
+        self.terminal.disable_bill_acceptor()
+        self.frame.columnconfigure(0, weight=0)
+        for btn in self.types:
+            btn.destroy()
+        self.types = []
+        for org in self.elements:
             org.destroy()
+        self.elements = []
 
 
 class CheckPrinter(Frame):
@@ -180,9 +238,34 @@ class BillAcceptor(Frame):
         self.entry = Entry(self.rf, textvariable=self.var, state=DISABLED, width=10)
         self.entry.grid(row=0, column=0, sticky='nw', padx=3)
         Label(self.rf, text='rubles', width=10).grid(row=1, column=0, sticky='nw', padx=3)
+        self.state = 0
 
     def insert(self):
         print('Not implemented yet!')
+
+    def activate(self):
+        self.insert_cash.config(state=NORMAL)
+        self.entry.config(state=NORMAL)
+        self.active = self.after(700, self.change_image)
+
+    def change_image(self):
+        if self.state:
+            print_img = get_resized_image(os.path.join('gif', 'bill_acceptor.gif'), self.width, self.width)
+            self.insert_cash.config(image=print_img)
+            self.insert_cash.image=print_img
+            self.state =0
+        else:
+            print_img = get_resized_image(os.path.join('gif', 'bill_acceptor_2.gif'), self.width, self.width)
+            self.insert_cash.config(image=print_img)
+            self.insert_cash.image = print_img
+            self.state = 1
+        self.active = self.after(700, self.change_image)
+
+    def disable(self):
+        self.insert_cash.config(state=DISABLED)
+        self.entry.config(state=DISABLED)
+        if hasattr(self, 'active'):
+            self.after_cancel(self.active)
 
 if __name__ == '__main__':
     t = TkTerminal(1049)
