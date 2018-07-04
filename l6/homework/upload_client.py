f180160be034991667264736980713d85183ddb4
Kirill C <cherkasov.kirill@gmail.com>
2018-07-04 11:32:07 +0500

    Replace run method on start for multiprocessing.

--- l6/homework/upload_client.py
+++ l6/homework/upload_client.py
@@ -4,33 +4,32 @@ import struct
 import multiprocessing as mp
 
 FILE_DIR = os.path.join(os.path.dirname(__file__), 'download')
-HOST, PORT = 'localhost', 5555
+HOST, PORT = '192.168.10.123', 5555
 if os.path.exists('upload/test.jpeg'):
     os.remove('upload/test.jpeg')
 
 
 def upload_file(scandir_obj):
     size = os.path.getsize(scandir_obj.path)
-    read_sz = size // 1000
     with open(scandir_obj.path, 'rb') as up_f:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect((HOST, PORT))
         action = 0x06   # action for file download
         name_len = len(scandir_obj.name)
-        header = struct.pack('BBQQ', action, name_len, read_sz, size)
+        header = struct.pack('BBQ', action, name_len, size)
         sock.sendall(header)
         sock.sendall(scandir_obj.name.encode('utf-8'))
         while True:
-            data = up_f.read(read_sz)
+            data = up_f.read(1024)
             if not data:
                 break
             sock.sendall(data)
             receive = sock.recv(struct.calcsize('f'))
-            print('{0:.1f}'.format(struct.unpack('f', receive)[0]))
+            # print('{0:.1f}'.format(struct.unpack('f', receive)[0]))
         sock.close()
 
 
 if __name__ == '__main__':
     for file in os.scandir(FILE_DIR):
         task = mp.Process(target=upload_file, args=(file, ))
-        task.run()
+        task.start()
--- l6/homework/uploadserv.py
+++ l6/homework/uploadserv.py
@@ -3,30 +3,30 @@ import struct
 import asyncio
 import datetime
 
-HOST, PORT = 'localhost', 5555
+HOST, PORT = '192.168.10.123', 5555
 UPLOAD_DIR = os.path.join(os.path.dirname(__file__), 'upload')
-test_fname = os.path.join(UPLOAD_DIR, 'test.jpeg')
 async def write(file, data):
     file.write(data)
 
 async def handle_request(reader, writer):
-    header = await reader.read(struct.calcsize('BBQQ'))
-    header, length, read_sz, size = struct.unpack('BBQQ', header)
-    if header == 0x06:  # Upload file
-        upload_name_binary = await reader.read(length)
-        upload_name = upload_name_binary.decode('utf-8')
-        upload_path = os.path.join(UPLOAD_DIR, upload_name)
+    header = await reader.read(struct.calcsize('BBQ'))
+    header, length, size = struct.unpack('BBQ', header)
+    if header != 0x06:  # Upload file
+        return
+    upload_name_binary = await reader.read(length)
+    upload_name = upload_name_binary.decode('utf-8')
+    upload_path = os.path.join(UPLOAD_DIR, upload_name)
     i = 0
     if os.path.exists(upload_path):
         os.remove(upload_path)
     print('Uploading... {} started at {}'.format(upload_name, datetime.datetime.now()))
     while True:
-        data = await reader.read(read_sz)
+        data = await reader.read(1024)
         if not data:
             break
         with open(upload_path, 'ab') as fup:
             fup.write(data)
-        uploaded_size = i * read_sz
+        uploaded_size = i * 1024
         upload_percentage = struct.pack('f', uploaded_size / size * 100)
         writer.write(upload_percentage)
         await writer.drain()
